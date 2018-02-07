93e6e8de080b6c3d6f9064a9061831c183a8ae9d
James Doss-Gollin <james.dossgollin@gmail.com>
2018-02-07 09:37:34 -0500

    update klee plot

--- src/analyze/plot_klee.py
+++ src/analyze/plot_klee.py
@@ -8,44 +8,49 @@ import argparse
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
-import colorcet as cc
 
 parser = argparse.ArgumentParser() #pylint: disable=C0103
 parser.add_argument("--outfile", help="the filename of the data to save")
 parser.add_argument("--wt", help="The Weather Type sequence")
 
-def main():
-    # Read in data
-    args = parser.parse_args()
-    wt = xr.open_dataarray(args.wt).to_dataframe()
+def get_year_adj(time):
+    """Given a datetime index, return the "year_adj" variable.
 
-    # Get the adjusted year and day of season
-    time = wt.index
-    year = time.year
-    month = time.month
+    This is the year that the NDJF season ended, so the period
+    Nov 1979 to Feb 1980 has a "year_adj" of 1980.
+    """
+    month = time.month.values
     year_adj = time.year.values
     year_adj[np.in1d(month, [11, 12])] += 1
-    wt['year_adj'] = year_adj
-    wt['doy'] = wt.groupby(year_adj).transform(lambda x: np.arange(x.size))['year_adj']
+    return year_adj
+
+def main():
+    """Run through the code
+    """
+    args = parser.parse_args()
+    w_type = xr.open_dataarray(args.wt).to_dataframe()
+    w_type['year_adj'] = get_year_adj(w_type.index)
+    w_type['doy'] = w_type.groupby(w_type['year_adj']).transform(
+        lambda x: np.arange(x.size))['year_adj']
 
     # Make klee diagram and annual proportion diagram
-    wt_ds = wt.set_index(['doy', 'year_adj']).to_xarray()['wtype']
-    wt_prop = wt.groupby(['year_adj', 'wtype']).size().to_xarray()
+    wt_ds = w_type.set_index(['doy', 'year_adj']).to_xarray()['wtype']
+    wt_prop = w_type.groupby(['year_adj', 'wtype']).size().to_xarray()
     wt_prop = wt_prop / wt_prop.sum(dim='wtype')
     wt_prop = wt_prop.to_pandas()
 
     # set up the figure
-    fig,axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
     ax = axes[0]
     wt_ds.plot(x='doy', y='year_adj', ax=ax, add_colorbar=False)
     ax.set_xlabel('Days Since 01 November')
     ax.set_ylabel('Year')
     ax = axes[1]
-    wt_prop.plot.barh(stacked=True, cmap='viridis', ax=ax)
+    wt_prop.plot.barh(stacked=True, cmap='viridis', ax=ax, width=0.9)
     ax.set_xlabel('Proportion of Days')
     ax.set_ylabel('')
     ax.set_yticks([])
-    plt.tight_layout()
+    fig.tight_layout()
     plt.savefig(args.outfile, bbox_inches='tight')
 
 if __name__ == "__main__":
